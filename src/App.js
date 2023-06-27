28b334094c26eea7d7ae9951147c1e6c15be4659
Kayc <ghstkc@gmail.com>
2023-06-27 16:42:29 +0545

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signup from './views/Signup/Signup';
+import Home from './views/Home/Home';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return (<
+        div className='main' >
+        <Router >
+        <Routes >
+            <Route path="/" element={<Home/>} />
+            <Route path="/signup" element={<Signup/>} />
+        </Routes> 
+        </Router>
+       </div>
+                );
+};
 
-export default App
+export default App;
