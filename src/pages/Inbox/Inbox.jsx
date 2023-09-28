7c86096a906cfd154b0db86607b7008e7de39f72
Kayc <ghstkc@gmail.com>
2023-09-28 13:29:04 +0545

    update Inbox

--- src/pages/Inbox/Inbox.jsx
+++ src/pages/Inbox/Inbox.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { getInbox } from '../../api/messages';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import './Inbox.scss';
 
 
@@ -18,35 +18,37 @@ const Inbox = () => {
         console.error('Error fetching inbox messages:', error);
       });
   };
-       useEffect(() => {
-        // Fetch initial products data
-        return () => fetchInbox();
-      }, []);
-
-      return (
-        <div className='container mt-4'>
-          <h2 className='mb-4'>Inbox</h2>
-          <div className='list-group'>
-            {messages.map((message) => (
-              <Link
-                key={message.id}
-                to={`/chat/${message.username}`}
-                className='list-group-item list-group-item-action'
-              >
-                <div className='d-flex w-100 justify-content-between'>
-                  <h5 className='mb-1'>{message.username}</h5>
-                  <small>{new Date(message.created_at).toLocaleDateString()}</small>
-                </div>
-                <p className='mb-1'>
-                  {message.content.length > 50
-                    ? `${message.content.slice(0, 50)} ...`
-                    : message.content}
-                </p>
-              </Link>
-            ))}
-          </div>
-        </div>
-      );
+  useEffect(() => {
+    return () => {
+      fetchInbox();
     };
-    
+  }, []);
+  return (
+    <div className='container-fluid'>
+      <h2 className='mb-4'>Inbox</h2>
+      <div className='list-group'>
+        <div className='scrollable-content'>
+          {messages.map((message) => (
+            <Link
+              key={message.id}
+              to={`/chat/${message.username}`}
+              className='list-group-item list-group-item-action mb-3'
+            >
+              <div className='d-flex w-100 justify-content-between'>
+                <h5 className='mb-1'>{message.username}</h5>
+                <small>{new Date(message.created_at).toLocaleDateString()}</small>
+              </div>
+              <p className='mb-1'>
+                {message.content.length > 100
+                  ? `${message.content.slice(0, 100)} ...`
+                  : message.content}
+              </p>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Inbox;
