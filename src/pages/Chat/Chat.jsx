50d09eed633fd4cc3e11e4756176748eb6a1c4ab
Kayc <ghstkc@gmail.com>
2023-09-28 13:26:39 +0545

    Modify chat ui

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,15 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import { getMessages, sendMessage } from '../../api/messages';
-import { useParams,Link } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import './Chat.scss';
 
-const Chat = ({}) => {
-    const { username } = useParams();
-    const [messages, setMessages] = useState([]);
-    const [newMessage, setNewMessage] = useState('');
+const Chat = ({ }) => {
+  const { username } = useParams();
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
 
-    useEffect(() => {
-        // Fetch chat messages when the component mounts
+  const fetchChat = () => {
+    // Fetch inbox messages when the component mounts
+    getMessages(username)
+      .then((data) => {
+        setMessages(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching chat messages:', error);
+      });
+  };
+
+  useEffect(() => {
+    // Fetch chat messages when the component mounts
+    return () => {
+      fetchChat();
+    };
+  }, [username]); // Re-fetch messages when the username changes
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+    // Send a new message
+    const trimmedMessage= newMessage.trim();
+
+    if (trimmedMessage === '') {
+      return;
+    }
+
+    const messageData = {
+      receiver_username: username,
+      content: trimmedMessage,
+    };
+
+    sendMessage(messageData)
+      .then(() => {
+        // Clear the input field and fetch updated messages
+        setNewMessage('');
         getMessages(username)
           .then((data) => {
             setMessages(data);
@@ -17,73 +52,61 @@ const Chat = ({}) => {
           .catch((error) => {
             console.error('Error fetching chat messages:', error);
           });
-      }, [username]); // Re-fetch messages when the username changes
-    
-      const handleSendMessage = () => {
-        // Send a new message
-        const messageData = {
-          receiver_username: username,
-          content: newMessage,
-        };
-    
-        sendMessage(messageData)
-          .then(() => {
-            // Clear the input field and fetch updated messages
-            setNewMessage('');
-            getMessages(username)
-              .then((data) => {
-                setMessages(data);
-              })
-              .catch((error) => {
-                console.error('Error fetching chat messages:', error);
-              });
-          })
-          .catch((error) => {
-            console.error('Error sending message:', error);
-          });
-      };
-    
-      return (
-        <div className='container mt-4 chat-container'>
-          <div className='chat-header'>
-            <Link to="/inbox" className='btn btn-secondary mb-3'>Back to Inbox</Link>
-            <h2>{username}'s Chat</h2>
-          </div>
-          <div className='chat-box'>
-            <div className='chat-messages'>
-              {messages.map((message) => (
-                <div
-                  key={message.id}
-                  className={`message ${
-                    message.sender_id === username ? 'received' : 'sent'
-                  }`}
-                >
+      })
+      .catch((error) => {
+        console.error('Error sending message:', error);
+      });
+  };
+
+  return (
+    <div className='container-fluid'>
+      <div className='chat-header'>
+        <Link to="/inbox" className='btn btn-secondary mb-3'>Inbox</Link>
+        <h2>{username}'s Chat</h2>
+      </div>
+      <div className='chat-box'>
+        <div className='chat-messages list-group'>
+          <div className='scrollable-content-chat'>
+            {messages.map((message) => (
+              <div
+                key={message.id}
+                className='message list-group-item'
+              >
+                <div className={`${message.sent ? 'sent' : 'received'}`}>
+                <div className="message-data">
                   <p>{message.content}</p>
+                  {/* <p>{`${new Date(message.created_at).toLocaleDateString()} ${new Date(message.created_at).toLocaleTimeString()}`}</p> */}
+                  <p className='time'>{`${new Date(message.created_at).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })} `}</p>
+                  <p className='time'>{`${new Date(message.created_at).toLocaleDateString()} `}</p>
+
                 </div>
-              ))}
-            </div>
-            <div className='input-box'>
-              <div className='input-group'>
-                <input
-                  type='text'
-                  className='form-control'
-                  placeholder='Type a message...'
-                  value={newMessage}
-                  onChange={(e) => setNewMessage(e.target.value)}
-                />
-                <div className='input-group-append'>
-                  <button
-                    className='btn btn-primary'
-                    type='button'
-                    onClick={handleSendMessage}
-                  >
-                    Send
-                  </button>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
-      );
-    };
+        <form onSubmit={handleSendMessage}>
+          <div className='input-box'>
+            <input
+              type='text'
+              className='form-control'
+              placeholder='Type a message...'
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+            />
+            <div className='input-group-append'>
+              <button
+                className='btn btn-primary'
+                type='submit'
+              >
+                Send
+              </button>
+            </div>
+          </div>
+        </form>
+
+      </div>
+    </div>
+  );
+};
 export default Chat;
